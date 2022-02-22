9a84b53aa9e026833b6516895bb0006db49ef184
下塘烧饼 <zhaochun.hefei@qq.com>
2022-02-22 18:30:10 +0800

    基础版本代码提交

---
+++ lib/server/db/factory/factory.go
@@ -0,0 +1,47 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package factory
+
+import (
+	"context"
+
+	"gitee.com/zhaochuninhefei/fabric-ca-gm/lib/server/db"
+	"gitee.com/zhaochuninhefei/fabric-ca-gm/lib/server/db/mysql"
+	"gitee.com/zhaochuninhefei/fabric-ca-gm/lib/server/db/postgres"
+	"gitee.com/zhaochuninhefei/fabric-ca-gm/lib/server/db/sqlite"
+	"gitee.com/zhaochuninhefei/fabric-ca-gm/lib/tls"
+	"gitee.com/zhaochuninhefei/fabric-gm/bccsp"
+	"github.com/pkg/errors"
+)
+
+// DB is interface that defines the functions on a database
+type DB interface {
+	Connect() error
+	PingContext(ctx context.Context) error
+	Create() (*db.DB, error)
+}
+
+// New returns a DB interface for the request database type
+func New(
+	dbType,
+	datasource,
+	caName string,
+	tlsConfig *tls.ClientTLSConfig,
+	csp bccsp.BCCSP,
+	metrics *db.Metrics,
+) (DB, error) {
+	switch dbType {
+	case "sqlite3":
+		return sqlite.NewDB(datasource, caName, metrics), nil
+	case "postgres":
+		return postgres.NewDB(datasource, caName, tlsConfig, metrics), nil
+	case "mysql":
+		return mysql.NewDB(datasource, caName, tlsConfig, csp, metrics), nil
+	default:
+		return nil, errors.Errorf("Invalid db.type in config file: '%s'; must be 'sqlite3', 'postgres', or 'mysql'", dbType)
+	}
+}
---
+++ tools/go.mod
@@ -0,0 +1,17 @@
+module tools
+
+go 1.17
+
+require (
+	gitee.com/zhaochuninhefei/fabric-gm v0.0.1
+	github.com/AlekSi/gocov-xml v1.0.0
+	github.com/axw/gocov v1.0.0
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616
+	golang.org/x/tools v0.1.9
+)
+
+require (
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
